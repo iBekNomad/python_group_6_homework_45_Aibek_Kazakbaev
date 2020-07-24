4b56547bef18d7d299847a80a0ce2e302fe90e02
Aibek <nomad.aibek@gmail.com>
2020-07-24 17:31:51 +0600

    updated dates.2

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -10,7 +10,7 @@ STATUS_CHOICES = [
 class Article(models.Model):
     description = models.CharField(max_length=200, null=False, blank=False, verbose_name='Описание')
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='Новая')
-    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    created_at = models.DateField(auto_now_add=False, default='', verbose_name='Дата создания')
     updated_at = models.DateField(auto_now=True, verbose_name='Дата изменения')
 
     def __str__(self):
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -17,7 +17,8 @@ def task_create_view(request):
     elif request.method == 'POST':
         description = request.POST.get('description')
         status = request.POST.get('status')
-        article = Article.objects.create(description=description, status=status)
+        date = request.POST.get('created_at')
+        article = Article.objects.create(description=description, status=status, created_at=date)
         context = {'article': article}
         return render(request, 'task_view.html', context)
 
